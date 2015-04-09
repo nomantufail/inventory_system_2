3b905e8ae0ef5e780b5224ede92f3e8ee7bf3430
nomantufail <nomantufail100@gmail.com>
2015-04-09 08:26:49 -0700

    noman

---
+++ application/controllers/test_controller_3.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+include_once(APPPATH."controllers/parentController.php");
+class Test_Controller extends ParentController {
+    //public variables...
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
